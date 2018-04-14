2ff048b8da9b76bd0fd0d6044b7e15678fc93dcc
Siddhant Patil <siddhantpatil17@gmail.com>
2018-04-13 22:01:15 -0700

    Add files via upload

--- index_angular.js
+++ index_angular.js
@@ -1,204 +1,194 @@
 var Myapp = angular.module('MySkyCast', ['ngResource']);
 
-
-/*
-if (localStorage.getItem("history") === null) {
-  var cities = [];
-  localStorage.setItem("history", JSON.stringify(cities));
-}
-*/
-
-Myapp.controller('MainController',['$scope', '$resource' ,'$window',  function($scope, $resource){
+Myapp.controller('MainController',['$scope', '$resource' ,'$window',  function($scope, $resource) {
 	
 	
-	//window.alert("Loaded Controller");
-	$scope.currentCoordinates={};
-        $scope.weather = undefined;
-	//$scope.inputCity = "";
-	
-	//$scope.historyOptions = JSON.parse(localStorage.getItem("history"));
-    	$scope.getCoordinates = function(inputCity) {
-        	$scope.inputCity = inputCity;
-		
-		window.alert("You entered: "+ $scope.inputCity);
-		
-		var res = $resource('/dynamic/geocoding/:query', {query: $scope.inputCity});
-		window.alert(" Done1 ");
-
-		$scope.currentCoordinates = res.get(function() {
-
-  		 	window.alert("Successful Coordinates "+ $scope.inputCity); 
-			$scope.displayCity = $scope.inputCity;
-			getWeather();	
-		
-		}, function(){
-		});
-
-		window.alert(" Done ");     	
-
+	$scope.displayCity = "";
+    $scope.weather = undefined;
+    var weatherIconMap = {
+		'clear-day': 'day-sunny',
+		rain: 'rain',
+		snow: 'snow',
+		'clear-night': 'night-clear',
+		wind: 'strong-wind',
+		fog: 'fog',
+		sleet: 'sleet',		
+		'partly-cloudy-day': 'day-cloudy',
+		cloudy: 'cloudy',
+		'partly-cloudy-night': 'night-cloudy'
 	};
 
+	Highcharts.setOptions({
+  		global: {
+    		useUTC: false
+  		}
+	});
 
-	function getWeather() {
+	var input = document.getElementById('searchTextField');
+  	var autocomplete = new google.maps.places.Autocomplete(input); 
+	autocomplete.addListener('place_changed', function() {
+			getCoordinates();
+		});
+
+
+
+
+	// OBTAIN LATITUDE AND LONGITUDE VALUES
+    function getCoordinates() {
 		
-		window.alert("You entered getWeather Function() ");
+		var place = autocomplete.getPlace();
+		var str = String(place.geometry.location);
+		var co_ordinates = str.split(",");
+		var latitude = co_ordinates[0].replace("(", "").trim();
+		var longitude = co_ordinates[1].replace(")", "").trim();
+		$scope.displayCity = JSON.stringify(place.formatted_address).replace(/"/g, '');   
+		getWeather(latitude, longitude);
+  	
+	}
 
-		var res = $resource('/dynamic/forecast/' + $scope.currentCoordinates.geometry.location.lat +
-				',' + $scope.currentCoordinates.geometry.location.lng);
-		$scope.weather = res.get(function(){
 
-			window.alert("Successful Forecast "+ $scope.inputCity); 
+	// CALL TO BACKEND WITH LATITUDE AND LONGITUDE VALUES, GETS BACK DARKSKY WEATHER
+	function getWeather(latitude, longitude) {
+		
+		var res = $resource('/backend/forecast/' + latitude +',' + longitude);
+		$scope.weather = res.get(function() {
 			showWeather();
-			//addToHistory();
-
-		}, function(){
+			addToHistory();
+		}, function() {
+			//TODO Error
 		});
 
-		window.alert(" Done Forecast ");
-		
 	};
 
 
-
-
-
-
-	/**Returns the argument unless it is undefined, in which case this returns "precipitation".*/
+	// PRECIPITATION FIELD DISPLAY FUNCTION
 	$scope.precipType = function(str) {
+
 		if(str === undefined) {
 			return "precipitation";
 		}
 		return str;
-	}
-	
-	
-	var weatherIconMap = {
-		'clear-day': 'day-sunny',
-		'clear-night': 'night-clear',
-		rain: 'rain',
-		snow: 'snow',
-		sleet: 'sleet',
-		wind: 'strong-wind',
-		fog: 'fog',
-		cloudy: 'cloudy',
-		'partly-cloudy-day': 'day-cloudy',
-		'partly-cloudy-night': 'night-cloudy'
-	};
 
-	/*Takes an icon string from the Dark Sky Forecast API
-	and converts it into one of our weather icon classes.*/
+	}
+
+
+	// RENDER APPROPRIATE WEATHER ICONS
 	$scope.getWeatherIconClass = function(icon) {
+
 		var result = weatherIconMap[icon];
-		if(result === undefined) {
-			result = 'alien';//fun
-		}
 		return 'wi-' + result;
+
 	}
-
-	Highcharts.setOptions({
-  		global: {
-    			useUTC: false
-  			}
-	});
-
-	function FormatNumberLength(num) 
-	{
-		
-    		var r = "" + num;
-    		while (r.length < 13) 
-		{
-        		r = r + "0";
-    		}
 	
-    	return parseInt(r);
-	}
 
+	// FORMAT UTC TEMPERATURE VALUES FOR HOURLY CHART
+	function FormatNumberLength(num) {
+
+    	var r = "" + num;
+    	while (r.length < 13) {
+        	r = r + "0";
+    	}
+    	return parseInt(r);
+
+	}
+	
+
+	// BUILD AND DISPLAY HOURLY CHART
 	function showWeather() {
 
 		var hourlyWeather = [];
 		var hourlyTemperature = [];
 		var next24Hours = $scope.weather.hourly.data.slice(0,24);
-		for(var i=0; i<next24Hours.length; i++)
-		{
+
+		for(var i=0; i<next24Hours.length; i++) {
 			var tempArray = [];
 			tempArray.push(FormatNumberLength(next24Hours[i].time));
 			tempArray.push(Math.round(next24Hours[i].temperature));
 			hourlyTemperature.push(Math.round(next24Hours[i].temperature));
 			hourlyWeather.push(tempArray);
 		}
-		//window.alert(hourlyTemperature);
-		//window.alert(Math.min.apply(Math,hourlyTemperature));
+		
 		Highcharts.chart('weatherGraph', {
-		chart: {
+			chart: {
         		type: 'area'
     		},
-		exporting: { enabled: false },
-		title: {
+			exporting: { enabled: false },
+			title: {
         		text: null
     		},
-		plotOptions: {
+			plotOptions: {
         		series: {
-            			dataLabels: {
-                			enabled: true
-            			}
+            		dataLabels: {
+                		enabled: true
+            		}
         		}
     		},
-		yAxis: {
-			visible: false,
-			max:Math.max.apply(Math,hourlyTemperature)+5,
-			min:Math.min.apply(Math,hourlyTemperature)-5,
-		},
-		xAxis: {
-			type: 'datetime',
-   			title: {
+			yAxis: {
+				visible: false,
+				max:Math.max.apply(Math,hourlyTemperature)+5,
+				min:Math.min.apply(Math,hourlyTemperature)-5,
+			},
+			xAxis: {
+				type: 'datetime',
+   				title: {
     				enabled: false
-   			}
-		},
-		series: [{ 
-			name:'°F' ,
-   			showInLegend: false,
-   			data: hourlyWeather,
-			color: '#FFFF99'
+   				}
+			},
+			series: [{ 
+				name:'°F' ,
+   				showInLegend: false,
+   				data: hourlyWeather,
+				color: '#FFFF99'
 			}],
-			
 		});
-	
+
 	}
 	
-	/*
+	
+	// STORE SEARCH QUERY IN LOCAL STORAGE
+	function addToHistory() {
 
-	function addToHistory(){
-		
-		window.alert("In addToHistory"+ $scope.inputCity);
+		if (localStorage.getItem("history") === null) {
+  			var cities = [];
+  			localStorage.setItem("history", JSON.stringify(cities));
+		}
 		var history = JSON.parse(localStorage.getItem("history"));
-		window.alert("In addToHistory"+history);
-		history.push($scope.inputCity);
-		window.alert("In addToHistory"+history);
+		history.push($scope.displayCity);
 		localStorage.setItem("history", JSON.stringify(history));
-		$scope.historyOptions = JSON.parse(localStorage.getItem("history"));
-		retrieveHistory();
 
 	}
 
-	function retrieveHistory(){
 
-		window.alert("In retrieveHistory");
-		var h = JSON.parse(localStorage.getItem("history"));
-		window.alert("In 2retrieveHistory"+h);
-		for (var j = 0; j < h.length; j++) {
-  			window.alert(h[j]);
+	// RETRIEVE & DISPLAY PREVIOUS SEARCH QUERIES
+	$scope.retrieveHistory = function() {
+
+		if (localStorage.getItem("history") === null) {
+			$scope.historyOptions = [];
+		}
+		else{
+			$scope.historyOptions = JSON.parse(localStorage.getItem("history"));
 		}
 
 	}
-		
-	function clearHistory(){
+
+
+	// CLEAR SEARCH QUERY HISTORY
+	$scope.clearHistory = function() {
 			
-		window.alert("In Clear History");
-		//localStorage.clear();
+		localStorage.removeItem("history");
+		$scope.historyOptions = null;
 	
 	}
 
-	*/
+
+	// SEARCH QUERY FROM HISTORY
+	$scope.dropboxItemSelected = function(city) {
+			
+		document.getElementById('searchTextField').value="";
+		document.getElementById('searchTextField').value=city; 
+	
+	}
+	
 }]);
 
 
